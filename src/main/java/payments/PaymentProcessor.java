d33fc9470a4cb60c34efb3caa387ac767a3e1ae5
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-31 14:09:19 -0600

    Creating test for Paymentent Processor version 1

---
+++ src/main/java/payments/PaymentProcessor.java
@@ -0,0 +1,18 @@
+package payments;
+
+public class PaymentProcessor {
+    private PaymentGateway paymentGateway;
+
+    public PaymentProcessor(PaymentGateway paymentGateway) {
+        this.paymentGateway = paymentGateway;
+    }
+
+    public boolean makePayment(double amount){
+        PaymentResponse response = paymentGateway.requestPayment(new PaymentRequest(amount));
+        if (response.getStatus() == PaymentResponse.PaymentStatus.OK){
+           return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/payments/PaymentProcessorTest.java
@@ -0,0 +1,31 @@
+package payments;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class PaymentProcessorTest {
+    @Test
+    public void payment_is_correct(){
+        // Simulated the interface
+        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
+        Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
+
+        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
+        assertTrue(paymentProcessor.makePayment(1000));
+    }
+
+    @Test
+    public void payment_is_error(){
+        // Simulated the interface
+        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
+        Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
+
+        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
+        assertFalse(paymentProcessor.makePayment(1000));
+    }
+
+}
