3398fdfd0458cbf8445c0d780a7095533bd09306
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-31 23:08:33 -0600

    Creating project payments and use of mockito,setUp and test

--- src/test/java/payments/PaymentProcessorTest.java
+++ src/test/java/payments/PaymentProcessorTest.java
@@ -1,30 +1,39 @@
 package payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+
+
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup(){
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void payment_is_correct(){
-        // Simulated the interface
-        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
+        // Preparation escenario
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+        // LLamada al metodo a probar.
+        boolean result =paymentProcessor.makePayment(1000);
 
-        assertTrue(paymentProcessor.makePayment(1000));
+        // Comprobación del resultadoe sperado
+        assertTrue(result);
     }
 
     @Test
     public void payment_is_error(){
         // Simulated the interface
-        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-
         assertFalse(paymentProcessor.makePayment(1000));
     }
 
